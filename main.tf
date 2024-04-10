c353d45e5acfc432768d2b31c4b894019db56ecf
mktechph <114472493+mktechph@users.noreply.github.com>
2024-04-10 16:20:54 +0800

    first commit

---
+++ main.tf
@@ -0,0 +1,8 @@
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr_block
+
+  tags = {
+    Name = "Main"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "vpc_cidr_block" {
+  description = "CIDR Block of the VPC."
+  type = string
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC."
+  type = string
+}
+
